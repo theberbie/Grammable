d0cfdf1b785aed3cd48bdac1398a9c69f7c5ebc2
vagrant <vagrant@precise32.(none)>
2016-06-04 06:43:58 +0000

    Wrote comment validation tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+
+  def render_not_found(status=:not_found)
+    render text: "#{status.to_s.titleize} :(", status: status
+  end
+
   protect_from_forgery with: :exception
 end
--- app/models/gram.rb
+++ app/models/gram.rb
@@ -6,6 +6,6 @@ class Gram < ActiveRecord::Base
 
 
   belongs_to :user
-
+  has_many :comments
   
 end
